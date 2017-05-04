6449b9e3e2880291736b3f978c1e88945cfd64b4
ereborlugimli <yenidoganbusra@gmail.com>
2017-05-04 12:47:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, resp) {
-    resp.send("Benim Web Sitem"); //kullanıcılara görünecek yazı
+    resp.send("Web Site"); //kullanıcılara görünecek yazı
 });
 
 app.listen(8080); //uygulamamız bu portu dinleyecek. http://localhost:8080
